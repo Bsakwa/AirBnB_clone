b720a88c971e6703df0852019a1cc3b14035fc6c
Bsakwa <sakwabrian23@gmail.com>
2023-02-08 12:38:39 +0000

    Add class city

---
+++ models/city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+"""Defines a City Class"""
+from models.base_models import BaseModel
+
+
+class City(BaseModel):
+    """
+    Represents a city
+
+    Attributes:
+        state_id (str): The state id of the city
+        name (str): The name of the city
+    """
+
+    state_id = ""
+    name = ""
