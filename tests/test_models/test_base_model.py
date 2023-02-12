d6d5637ef8aa3d194f70199bda13544f5c75dd67
Bsakwa <sakwabrian23@gmail.com>
2023-02-12 09:53:45 +0000

    Fixed pycode

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+"""Defines classes for models/base_model.py.
+
+Unittest classes:
+    TestBModel_instantiation
+    TestBmodel_save
+    TestBmodel_to_dict
+"""
+import models
+import os
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from time import sleep
+
+
+class TestBModel_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel"""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test_two_models_unique_ids(self):
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
+
+    def test_two_models_different_created_at(self):
+        bm1 = BaseModel()
+        sleep(0.05)
+        bm2 = BaseModel()
+        self.assertLess(bm1.created_at, bm2.created_at)
+
+    def test_two_models_different_updated_at(self):
+        bm1 = BaseModel()
+        sleep(0.05)
+        bm2 = BaseModel()
+        self.assertLess(bm1.updated_at, bm2.updated_at)
+
+
+if __name__ == '__main__':
+    unittest.main()
