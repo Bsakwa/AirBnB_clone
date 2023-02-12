3d3162a5348d4a3ad9ec8391229a3554ace99b45
Bsakwa <sakwabrian23@gmail.com>
2023-02-12 09:18:01 +0000

    Unittests for command interpreter

---
+++ tests/test_console.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+""" Defines unittests for our AirBnB console
+
+Unitest classes:
+    TestConsole_prompt
+    TestConsole_help
+    TestConsole_exit
+    TestConsole_create
+    TestConsole_show
+    TestConsole_all
+    TestConsole_destroy
+    TestConsole_update
+"""
+import sys
+import unittest
+from models import storage
+from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
+from models.engine.file_storage import FileStorage
+import os
+
+class TestConsole_prompt(unittest.TestCase):
+    """ Unittests for testing the prompt of our command interpreter"""
+
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb)", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
+
+
+class TestConsole_help(unittest.TestCase):
+    """Unittests for testing the help function of the command interpreter
+    """
+
+    def test_help_quit(self):
+        prints = "Quit command to exit the interpreter"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help quit"))
+            self.assertEqual(prints, output.getvalue().strip())
+
+    def test_help_create(self):
+        prints = ("Usage: create <class>\n        "
+                  "Creates a new instance and prints it's id")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help create"))
+            self.assertEqual(prints, output.getvalue().strip())
+
+    def test_help_EOF(self):
+        prints = "EOF signal to quit the program"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help EOF"))
+            self.assertEqual(prints, output.getvalue().strip())
+
+    def test_help_show(self):
+        prints = ("Usage: show <class> <id>\n        "
+                  "Prints the string representation of an instance")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help show"))
+            self.assertEqual(prints, output.getvalue().strip())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
